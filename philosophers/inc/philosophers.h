ba6d8c1252ccd8d7b9a6625c684d40a6786e4237
Heath <heathcliffdogar@gmail.com>
2023-12-05 17:09:26 +0100

    Upload

--- philosophers/inc/philosophers.h
+++ philosophers/inc/philosophers.h
@@ -1,12 +1,27 @@
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 # include "../lib/libft/libft.h"
+# include <pthread.h>
+# include <sys/time.h>
 
 typedef struct s_conditions {
-	int	t_eat;
-	int	t_die;
-	int	t_sleep;
-	int	n_phil;
-	int	req_eat;
+	u_int64_t		teat;
+	u_int64_t		tdie;
+	u_int64_t		tsleep;
+	int				n_phil;
+	int				req_eat;
+	int				dead;
+	int				finished;
+	pthread_t		*tpid;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	writing;
+	t_phil			*phil;
 }				t_conditions;
+
+typedef struct s_phil {
+	int				id;
+	pthread_t		thread;
+	pthread_mutex_t	*fork_l;
+	pthread_muted_t	*fork_r;
+}				t_phil;
 #endif
--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -6,40 +6,136 @@
 /*   By: hdorado- <hdorado-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 17:13:49 by hdorado-          #+#    #+#             */
-/*   Updated: 2023/12/01 18:31:41 by hdorado-         ###   ########.fr       */
+/*   Updated: 2023/12/05 17:00:11 by hdorado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
 
+/*Things I might need to solve:
+-Does it matter if the time to eat/die/etc is bigger than an int, or should I store it in u_int64_t directly?*/
+long int	ft_atoli(const char *str)
+{
+	long int	i;
+	long int	num;
+	long int	sign;
+
+	sign = 1;
+	num = 0;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = 10 * num + (str[i] - 48);
+		i++;
+	}
+	return (sign * num);
+}
+
 int	ft_valid_atoi(char *str)
 {
+	int	i;
 
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (str[i] || ft_atoli(str) > 2147483647)
+		return (0);
+	return (1);
 }
 
 int	ft_check_args(int argc, char **argv, t_conditions *rules)
 {
-	if (ft_valid_atoi(argv[1]))
-		(*rules).n_phil = ft_atoi(argv[1]);
+	if (ft_valid_atoi(argv[1]) && ft_atoi(argv[1]))
+		rules->n_phil = ft_atoi(argv[1]);
 	else
 		return (0);
-	if (ft_valid_atoi(argv[2]))
-		(*rules).t_die = ft_atoi(argv[2]);
+	if (ft_valid_atoi(argv[2]) && ft_atoi(argv[2]))
+		rules->tdie = (u_int64_t) ft_atoi(argv[2]);
 	else
 		return (0);
-	if (ft_valid_atoi(argv[3]))
-		(*rules).t_eat = ft_atoi(argv[3]);
+	if (ft_valid_atoi(argv[3]) && ft_atoi(argv[3]))
+		rules->teat = (u_int64_t) ft_atoi(argv[3]);
 	else
 		return (0);
-	if (ft_valid_atoi(argv[4]))
-		(*rules).t_sleep = ft_atoi(argv[4]);
+	if (ft_valid_atoi(argv[4]) && ft_atoi(argv[4]))
+		rules->tsleep = (u_int64_t) ft_atoi(argv[4]);
 	else
 		return (0);
-	if (ft_valid_atoi(argv[5]))
-		(*rules).req_eat = ft_atoi(argv[5]);
+	if (argc == 6)
+	{
+		if (ft_valid_atoi(argv[5]))
+			rules->req_eat = ft_atoi(argv[5]);
+		else
+			return (0);
+	}
 	else
-		return (0);
+		rules->req_eat = -1;
 	return (1);
+}
+
+void	ft_init_phil(t_conditions *rules)
+{
+	int	i;
+
+	i = 0;
+	while (i < rules->n_phil)
+	{
+		rules->phil[i].id = i + 1;
+		if (i == 0)
+			rules->phil[0].fork_l = &rules->forks[rules->n_phil - 1];
+		else
+			rules->phil[i].fork_l = &rules->forks[i - 1];
+		rules->phil[i].fork_r = &rules->forks[i];
+		i++;
+	}
+}
+
+int	ft_alloc(t_conditions *rules)
+{
+	int	i;
+
+	i = 0;
+	rules->tpid = ft_calloc(sizeof(pthread_t), rules->n_phil);
+	if (!rules->tpid)
+		return (0);
+	rules->forks = ft_calloc(sizeof(pthread_mutex_t), rules->n_phil);
+	if (!rules->forks)
+		return (0);
+	while (i < rules->n_phil)
+	{
+		pthread_mutex_init(&rules->forks[i], NULL);
+		i++;
+	}
+	rules->phil = ft_calloc(sizeof(t_phil), rules->n_phil);
+	if (!rules->phil)
+		return (0);
+	ft_init_phil(rules);
+	return (1);
+}
+
+void	*ft_monitor(void *parameters)
+{
+	t_conditions	*rules;
+
+	rules = (t_conditions *)parameters;
+}
+
+int	ft_start(t_conditions *rules)
+{
+	pthread_create(&rules->tpid, NULL, ft_monitor, (void *)rules);
 }
 
 int	main(int argc, char **argv)
@@ -50,6 +146,11 @@ int	main(int argc, char **argv)
 	if (argc != 5 || argc != 6)
 		return (0);
 	i = 1;
+	rules.dead = 0;
+	rules.finished = 0;
+	pthread_mutex_init(&rules.writing, NULL);
 	if (!ft_check_args(argc, argv, &rules))
 		return (0);
+	ft_alloc(&rules);
+	ft_start(&rules);
 }
