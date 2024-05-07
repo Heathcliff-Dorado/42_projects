74a6c81d3e03bc0b10746b325602aec7e932274d
Heath <heathcliffdogar@gmail.com>
2024-05-07 14:18:03 +0200

    Changed BSP so it actually does BSP

--- Milestone_4/CPP02/ex03/bsp.cpp
+++ Milestone_4/CPP02/ex03/bsp.cpp
@@ -1,6 +1,6 @@
 #include "Point.hpp"
 
-Fixed	absolute( Fixed x ) {
+/*Fixed	absolute( Fixed x ) {
 	if (x < 0)
 		return (x * -1);
 	return (x);
@@ -19,6 +19,31 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point) {
 	if (Area1 == 0 || Area2 == 0 || Area3 == 0)
 		return (false);
 	if (realArea == (Area1 + Area2 + Area3))
+		return (true);
+	return (false);
+} */
+
+bool	IsInside(Point a, Point b, Point other, Point x) {
+	//We want to find the equation Ax + By + C = 0 for the points a and b:
+	Fixed A = a.getY() - b.getY();
+	Fixed B = b.getX() - a.getX();
+	Fixed C = a.getX() * b.getY() - a.getY() * b.getX();
+
+	//Check if other or x are in the same line as a and b
+	Fixed SolutionX = A * x.getX() + B * x.getY() + C;
+	Fixed SolutionOther = A * other.getX() + B * other.getY() + C;
+	if (SolutionX == 0 || SolutionOther == 0)
+		return (false);
+	if ((SolutionX > 0 && SolutionOther > 0) ||
+		(SolutionX < 0 && SolutionOther < 0))
+		return (true);
+	return (false);
+}
+
+bool	bsp( Point const a, Point const b, Point const c, Point const point) {
+	//We make a plane using two points, and check if the third and "point" are in the same side. We do this three times, following the lines a->b, b-> and c->a
+	//If the three points, or "point" are in the same line, then return false, as it will not be possible to be inside of the triangle
+	if(IsInside(a, b, c, point) && IsInside(b, c, a, point) && IsInside(c, a, b, point))
 		return (true);
 	return (false);
 }
