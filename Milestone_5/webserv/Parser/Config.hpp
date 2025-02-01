1e6fb6f815716672aae2cb91a2768207d89b24ca
Heath <heathcliffdogar@gmail.com>
2025-02-01 12:41:01 +0100

    Update webserv

--- Milestone_5/webserv/Parser/Config.hpp
+++ Milestone_5/webserv/Parser/Config.hpp
@@ -13,22 +13,23 @@
 class Config {
 	private:
 		Config( void );
-		void	parseConfigFile(const std::string &configFile);
+		std::vector<Server>	parseConfigFile(const std::string &configFile);
 
 		std::map<std::string, std::string>	_settings;
 		bool	_valid = false;
 
 		int	_port;
 		std::string	_root;
-		std::string	_allowed_methods;
-		std::vector<std::string>	_methods;
-		std::map<std::string, std::string> _error_pages;
 
 	public:
 		Config( const std::string& configFile);
 		Config( const Config& copy);
 		Config& operator=(const Config& copy);
 		~Config();
+};
+
+struct mylocations {
+	std::vector<std::string>	methods;
 };
 
 #endif
---
+++ Milestone_5/webserv/Parser/main.cpp
@@ -0,0 +1,8 @@
+#include "Config.hpp"
+
+int	main(int argc, char* argv){
+	if (argc != 2)
+	{
+		std::cout << "Error! Please include only a path to a config file" << std::endl;
+	}
+}
